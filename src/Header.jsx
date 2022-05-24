d16cf5bd031821c25b5e4155afc1b8ac20f7a2c4
zvilior <102918963+zvilior@users.noreply.github.com>
2022-05-24 19:33:46 +0300

    4z

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <h1>Welcome to the best playlist in the world</h1>
+
+      <Link to="/">Login||</Link>
+      <Link to="/list"> List||</Link>
+      <Link to="/search"> Search</Link>
+    </div>
+  );
+}
