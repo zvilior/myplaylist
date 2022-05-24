b919c105072aba9000e7c6f3242fdbc08fce7d64
zvilior <102918963+zvilior@users.noreply.github.com>
2022-05-24 18:28:49 +0300

    1z

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          plat with music
         </a>
       </header>
     </div>
