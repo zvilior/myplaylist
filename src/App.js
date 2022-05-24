c9f0f86ab750d9ebf8f33cbdba372152b3282a51
shayglik <glik99@gmail.com>
2022-05-24 18:37:12 +0300

    s2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Test from './components/Test';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
+          <Test/>
           plat with music
         </a>
       </header>
